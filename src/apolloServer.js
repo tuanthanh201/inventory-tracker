dbf4dd4a8bb4ccaa4ba4867aa85aa33ba00f2163
tuanthanh201 <55332021+tuanthanh201@users.noreply.github.com>
2022-05-12 20:52:06 -0400

    Updated commands for production

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const { ApolloServer } = require('apollo-server-express')
 
 const { typeDefs, resolvers } = require('./graphql')
@@ -39,6 +40,13 @@ const setupApolloServer = async () => {
       limit: '50mb',
     },
   })
+
+  if (process.env.ENV === 'PRODUCTION') {
+    app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    })
+  }
 
   return app.listen({ port: process.env.SERVER_PORT }, () => {
     console.log(`Server is up on port ${process.env.SERVER_PORT}`)
